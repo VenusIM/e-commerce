01e61faea36038b1018b216316dd2f14ecd3475c
imjunhui <yim3370@gmail.com>
2023-07-20 22:49:28 +0900

    E-Commerce user-service
    - gradle setting
    - add application.properties

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# e-commerse
+# e-commerce
 Spring cloud, Netflix eureka, Kafka 를 이용한  전자상거래 미니 프로젝트
---
+++ user-service/src/main/java/com/msa/userservice/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.msa.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class UserController {
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's working";
+    }
+}
