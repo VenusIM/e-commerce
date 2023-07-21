540831ed1c42d4cf7a1ad497dc4657d0813fcdd2
imjunhui <yim3370@gmail.com>
2023-07-21 22:10:12 +0900

    E-Commerce user-service
    - gradle setting h2, JPA

--- user-service/src/main/java/com/msa/userservice/controller/UserController.java
+++ user-service/src/main/java/com/msa/userservice/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.msa.userservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
 public class UserController {
 
-    @GetMapping("/health_check")
+    @GetMapping("health_check")
     public String status() {
-        return "It's working";
+        return "It's working in user-service";
     }
 }
