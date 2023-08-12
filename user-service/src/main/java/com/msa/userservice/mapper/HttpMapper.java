e33da1020b1bebfae5e3545f541c1c95caa57e08
imjunhui <yim3370@gmail.com>
2023-08-12 22:19:56 +0900

    E-Commerce user-service
    - Mapstruct Setter 매치가 안되는 이슈 수정
    - Security 를 이용하여 회원 인증에 성공한 경우 jwt 를 생성하여 Header Setting

--- user-service/src/main/java/com/msa/userservice/mapper/HttpMapper.java
+++ user-service/src/main/java/com/msa/userservice/mapper/HttpMapper.java
@@ -1,7 +1,6 @@
 package com.msa.userservice.mapper;
 
 import com.msa.userservice.dto.UserDTO;
-import com.msa.userservice.vo.request.RequestLogin;
 import com.msa.userservice.vo.request.RequestUser;
 import com.msa.userservice.vo.response.ResponseUser;
 import org.mapstruct.Mapper;
--- user-service/src/main/java/com/msa/userservice/security/AuthenticationFilter.java
+++ user-service/src/main/java/com/msa/userservice/security/AuthenticationFilter.java
@@ -9,7 +9,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
